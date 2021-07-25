663a6d84687ec632114b3d3489994933f0ba343e
patrick-moreira <patrickmoreira1995@gmail.com>
2021-07-25 11:01:38 -0300

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header';
 import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
